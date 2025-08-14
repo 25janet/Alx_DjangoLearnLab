c90986a5c07de593460805e9e61cd04aac3f5209
JANET WANJIRU JOHN <wanjirujay25@gmail.com>
2025-08-14 23:39:52 +0300

    Update settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -62,6 +62,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',       # add empty string
+        'PASSWORD': '',   # optional
+        'HOST': '',
+        'PORT': '',
     }
 }
 
