99d2e266787edc95772c5f89d837ea91f425fbdb
JANET WANJIRU JOHN <wanjirujay25@gmail.com>
2025-08-14 23:56:57 +0300

    Delete api directory

--- api/permmissions.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsVerifiedUser(BasePermission):
-    """
-    Allows access only to users with is_verified=True.
-    """
-
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and getattr(request.user, 'is_verified', False)
