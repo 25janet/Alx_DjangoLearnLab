5234020327077fbab02d0f1e2c1350a62a43c4a6
25janet <wanjirujay25@gmail.com>
2025-08-15 09:20:48 +0300

    Convert advanced_features_and_security from submodule to folder

---
+++ advanced_features_and_security/django-models/LibraryProject/bookshelf/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Book
+admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
+
+# Register your models here.
+# admin.py
+# accounts/admin.py
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django.utils.translation import gettext_lazy as _
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = list(UserAdmin.fieldsets) + [
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    ]
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+
+admin.site.register(CustomUser, CustomUserAdmin)
